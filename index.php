832bfed828e24e17b6b8f6f818eaa4b9acedb6af
Helmit-PC\Helmit <helmit2013@gmail.com>
2015-11-20 14:39:30 -0200

    asdf

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ body {text-align:center;margin:0}
 
 </head>
 <body background="http://i.imgur.com/8sGdTdf.png" style="background-repeat:repeat-x;">
-<div id="container">
+<div id="container">sdfaasfdas
 <div id="image1" style="position:absolute; overflow:hidden; left:21px; top:20px; width:336px; height:73px; z-index:0"><img src="http://i.imgur.com/H7Am8kL.png" alt="" title="" border=0 width=336 height=73></div>
 
 <div id="image3" style="position:absolute; overflow:hidden; left:0px; top:907px; width:948px; height:155px; z-index:1"><img src="http://i.imgur.com/ee2CygR.png" alt="" title="" border=0 width=948 height=155></div>
